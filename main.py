8ee581b922554f552b9625dafb9428ba58b6aac3
Cameron Frisby <zkac255@live.rhul.ac.uk>
2023-10-10 22:26:27 +0100

    Created method to view the image of a given digit from the dataset, split the sample into test and train

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import matplotlib
 import pandas as pd
+def display_image(digit):
+    #digit_image = digit.reshape(128)
+    plt.imshow(digit, cmap=matplotlib.cm.binary)
+    plt.axis("off")
+    plt.show()
 
 digits = load_digits()
 X_train, X_test, y_train, y_test = train_test_split(digits['data'],digits['target'], random_state=0)
 print(X_train.shape, X_test.shape)
-
+print(digits.target_names)
+display_image(digits.images[100])
