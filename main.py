6282e9a2ae45a0cd57baeb6c4b04a7078db2ad27
Cameron Frisby <zkac255@live.rhul.ac.uk>
2023-10-10 22:47:23 +0100

    Using K Nearest Neighbour Algorithm to train data, using the training data split from the dataset.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import numpy as np
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 import matplotlib
 import pandas as pd
+# Displays image of digit to user
 def display_image(digit):
     #digit_image = digit.reshape(128)
     plt.imshow(digit, cmap=matplotlib.cm.binary)
@@ -11,7 +13,12 @@ def display_image(digit):
     plt.show()
 
 digits = load_digits()
+# Split dataset into test and train
 X_train, X_test, y_train, y_test = train_test_split(digits['data'],digits['target'], random_state=0)
-print(X_train.shape, X_test.shape)
-print(digits.target_names)
-display_image(digits.images[100])
+
+# K Nearest Neighbour setup
+knn = KNeighborsClassifier(n_neighbors=10) # Need to test value of k, 10 for now
+# adding training data
+knn.fit(X_train, y_train)
+
+
